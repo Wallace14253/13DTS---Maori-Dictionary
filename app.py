913bdb4475280b3ef057e77911044c7263cad9af
18419 <18419@wc.school.nz>
2022-04-13 14:17:23 +1200

    Added categories and category pages

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-DATABASE = "MaoriDictionary.db"
+DATABASE = "identifier.sqlite"
 app.secret_key = "gjmbr'j'mybrb5yj3htivgha;'ej3q4238h"
 
 
@@ -24,16 +24,6 @@ def create_connection(db_file):
     return None
 
 
-@app.route('/')
-def home():
-    return render_template("Home.html")
-
-
-@app.route('/categories/<catagories>')
-def categories():
-    return render_template("home.html")
-
-
 def is_logged_in():
     if session.get("email") is None:
         print("not logged in")
@@ -42,8 +32,44 @@ def is_logged_in():
         print("logged in")
         return True
 
+
+def categories():
+    query = "SELECT id, Category_Name FROM Categories"
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query)
+    category = cur.fetchall()
+    con.close()
+    print(category)
+    return category
+
+
+
+@app.route('/')
+def home_page():
+    query = "SELECT id, Maori, English, Description, Level, image FROM Dictionary"
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query)
+    word_data = cur.fetchall()
+    con.close()
+
+    return render_template("Home.html", logged_in=is_logged_in(), words=word_data, categories=categories())
+
+
+@app.route('/categories/<category_id>')
+def categories_page(category_id):
+    query = "SELECT category_id, Maori, English, Description, Level, image FROM Dictionary"
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query)
+    word_data = cur.fetchall()
+
+    return render_template("categories.html", words=word_data, categories=categories(), category_id=int(category_id))
+
+
 @app.route('/login', methods=['POST', 'GET'])
-def login():
+def login_page():
     if is_logged_in():
         return redirect("/")
     if request.method == "POST":
@@ -77,11 +103,54 @@ def login():
         print(session)
         return redirect('/')
 
-    return render_template("Login.html", logged_in=is_logged_in())
+    return render_template("Login.html", logged_in=is_logged_in(), categories=categories())
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html', logged_in=is_logged_in())
+@app.route('/signup', methods=['POST', 'GET'])
+def signup_page():
+    if is_logged_in():
+        return redirect("/")
+    if request.method == 'POST':
+        print(request.form)
+        fname = request.form.get('fname').title().strip()
+        lname = request.form.get('lname').title().strip()
+        email = request.form.get('email').lower().strip()
+        password = request.form.get('password')
+        cpassword = request.form.get('cpassword')
+
+        if password != cpassword:
+            return redirect("/signup?error=Passwords+dont+match")
+        if len(password) < 8:
+            return redirect("/signup?error=Password+must+have+more+than+8+characters")
+
+        hashed_password = bcrypt.generate_password_hash(password)
+
+        con = create_connection(DATABASE)
+
+        query = "INSERT INTO Admin_logins (id, First_name, Last_name, Email, Password) VALUES(NULL, ?, ?, ?, ?)"
+
+        cur = con.cursor()
+        try:
+            cur.execute(query, (fname, lname, email, hashed_password))
+        except sqlite3.IntegrityError:
+            return redirect('/signup?error=email+is+already+used')
+        con.commit()
+        con.close()
+
+        return redirect("/login")
+
+    error = request.args.get("error")
+    if error == None:
+        error = ""
+
+    return render_template("Signup.html", error=error, logged_in=is_logged_in(), categories=categories())
+
+
+@app.route('/logout')
+def logout_page():
+    print(list(session.keys()))
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect('/?message=see+you+next+time!')
 
 
 if __name__ == '__main__':
